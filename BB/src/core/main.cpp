73f2f2ccbb76bd61fc0a33366ebd4fc993b3245a
tiziano-torterolo <83605541+tiziano-torterolo@users.noreply.github.com>
2025-07-18 20:09:47 +0000

    Hello i arrived

--- BB/src/core/main.cpp
+++ BB/src/core/main.cpp
@@ -1,7 +1,7 @@
 #include "../graphics/ventanaPrincipal.h"
 
 int main() {
-    BB::VentanaPrincipal ventana;
+    BB::VentanaPrincipal ventana;// hello, i arrived
 
     if (ventana.crear("BB - Motor", 800, 600)) {
         ventana.mostrarPorTiempo(8000);
